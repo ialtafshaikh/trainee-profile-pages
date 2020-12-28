b2c7717d31d4d02430f8ee28c5b29f5e7a161690
saurav <sourav.upadhyay@raweng.com>
2020-12-28 18:41:34 +0530

    Dummy data is added

---
+++ src/pages/dummy.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dummy extends Component {
+	render() {
+		return (
+			<div>
+				<h1>This is my dummy data</h1>
+			</div>
+		);
+	}
+}
+
+export default Dummy;
--- src/routes/router.js
+++ src/routes/router.js
@@ -5,19 +5,21 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AboutUs from "../pages/AboutUs";
 import Test from "../pages/test";
 import ContactUs from "../pages/ContactUs";
+import Dummy from "../pages/dummy";
 
 export default class Router extends Component {
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/about" exact component={AboutUs} />
-            <Route path="/contact" exact component={ContactUs} />
-            <Route path="/test" exact component={Test} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<Switch>
+						<Route path="/about" exact component={AboutUs} />
+						<Route path="/contact" exact component={ContactUs} />
+						<Route path="/test" exact component={Test} />
+						<Route path="/dummy" exact component={Dummy} />
+					</Switch>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
