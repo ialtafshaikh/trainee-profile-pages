0fa16ed5ffc290189e536ef8670792bfe9cc3993
Rishikesh46 <rishikesh.raut@raweng.com>
2020-12-29 17:54:10 +0530

    added card router

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,6 +8,7 @@ import AboutUs from "../pages/AboutUs";
 import Test from "../pages/test";
 import ContactUs from "../pages/ContactUs";
 import Test2 from "../pages/test2";
+import Cards from "../pages/Cards";
 
 export default class Router extends Component {
 	render() {
@@ -16,6 +17,7 @@ export default class Router extends Component {
 				<BrowserRouter>
 					<Switch>
 						<Route path="/about" exact component={AboutUs} />
+						<Route path="/cards" exact component={Cards} />
 						<Route path="/contact" exact component={ContactUs} />
 						<Route path="/test" exact component={Test} />
 						<Route path="/test2" exact component={Test2} />
