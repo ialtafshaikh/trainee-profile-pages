22bdeaf781f88afae47a0be5bf87dfa31b4253dc
Rishikesh46 <rishikesh.raut@raweng.com>
2020-12-30 12:50:00 +0530

    added Home route

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,7 +4,9 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //pages
+import Home from "../pages/Home"
 
+//components
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -16,6 +18,7 @@ export default class Router extends Component {
         <BrowserRouter>
           <Header />
           <Switch>
+            <Route path ="/" exact component={Home}/>
            
           </Switch>
           <Footer />
