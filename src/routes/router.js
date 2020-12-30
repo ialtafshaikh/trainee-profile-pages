39434fd6db04938df348185f163a345717c28fdd
Rishikesh46 <rishikesh.raut@raweng.com>
2020-12-30 11:15:58 +0530

    dummy pages deleted

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,11 +4,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //pages
-import AboutUs from "../pages/AboutUs";
-import Test from "../pages/test";
-import ContactUs from "../pages/ContactUs";
-import Test2 from "../pages/test2";
-import Cards from "../components/Cards";
+
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -19,11 +16,7 @@ export default class Router extends Component {
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route path="/about" exact component={AboutUs} />
-            <Route path="/cards" exact component={Cards} />
-            <Route path="/contact" exact component={ContactUs} />
-            <Route path="/test" exact component={Test} />
-            <Route path="/test2" exact component={Test2} />
+           
           </Switch>
           <Footer />
         </BrowserRouter>
