9aac6e2f6a1989065182d046fb0d6629bb09f333
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-30 10:42:17 +0530

    added footer and header to the routing

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,20 +8,26 @@ import AboutUs from "../pages/AboutUs";
 import Test from "../pages/test";
 import ContactUs from "../pages/ContactUs";
 import Test2 from "../pages/test2";
+import Cards from "../components/Cards";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default class Router extends Component {
-	render() {
-		return (
-			<div>
-				<BrowserRouter>
-					<Switch>
-						<Route path="/about" exact component={AboutUs} />
-						<Route path="/contact" exact component={ContactUs} />
-						<Route path="/test" exact component={Test} />
-						<Route path="/test2" exact component={Test2} />
-					</Switch>
-				</BrowserRouter>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/about" exact component={AboutUs} />
+            <Route path="/cards" exact component={Cards} />
+            <Route path="/contact" exact component={ContactUs} />
+            <Route path="/test" exact component={Test} />
+            <Route path="/test2" exact component={Test2} />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
