eefddc22aa45e3556722a248b83d8476b71f3264
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-31 12:27:43 +0530

    added url to call api

---
+++ src/components/ApiCall.jsx
@@ -0,0 +1,5 @@
+const url =
+  "https://trainee-profile-backend.herokuapp.com/employees/addEmployee";
+export default url;
+// http://localhost:3001/employees/addEmployee
+// http://localhost:4000/employees/addEmployee
