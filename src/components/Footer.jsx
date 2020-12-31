66e03a935926d77a7bddbdd5eea3bcefad657489
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-31 10:55:07 +0530

    Added icons using react-icon library

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import footerStyles from "../styles/Footer.module.css"
+import footerStyles from "../styles/Footer.module.css";
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <div id={footerStyles.copyright}>
-        <p>&copy; ExQuiSite. All rights reserved. | Design by Team ExQuiSite</p>
-        <ul className={footerStyles.contact}>
-          <li>
-            <a href="www.twitter.com"><span className="fa fa-twitter fa-2x"></span></a>
-          </li>
-          <li>
-            <a href="www.facebook.com"><span className="fa fa-facebook fa-2x"></span></a>
-          </li>
-          <li>
-            <a href="www.instagram.com"><span className="fa fa-instagram fa-2x"></span></a>
-          </li>
-        </ul>
-      </div>
+      <p>
+        &copy; Team NexT LeVeL. All rights reserved. | Design by Team NexT LeVeL
+      </p>
+      <ul className={footerStyles.contact}>
+        <li>
+          <a href="www.twitter.com">
+            <FaTwitter size="2em" color="cyan" />
+          </a>
+        </li>
+        <li>
+          <a href="www.facebook.com">
+            <FaFacebook size="2em" />
+          </a>
+        </li>
+        <li>
+          <a href="www.instagram.com">
+            <FaInstagram size="2em" />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
