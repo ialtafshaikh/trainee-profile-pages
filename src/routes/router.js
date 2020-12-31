25bc1469600c3304092f7840c7330517bb9b8efb
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-31 11:01:49 +0530

    implemented route path for the card detail with employee id

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,25 +4,26 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //pages
-import Home from "../pages/Home"
+import Home from "../pages/Home";
 
 //components
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import CardDetail from "../components/CardDetail";
 
 export default class Router extends Component {
   render() {
     return (
       <div>
+        <Header />
         <BrowserRouter>
-          <Header />
           <Switch>
-            <Route path ="/" exact component={Home}/>
-           
+            <Route path="/" exact component={Home} />
+            <Route path="/profile/:employeeId" exact component={CardDetail} />
           </Switch>
-          <Footer />
         </BrowserRouter>
+        <Footer />
       </div>
     );
   }
