66eb0d101a444ac0cf6f7faf5eb5f2b6bd0fe8d3
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-31 11:00:46 +0530

    added the card component on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import CardList from "../components/CardList"
+import React, { Component } from "react";
+import CardList from "../components/CardList";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-                <CardList />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Employee List</h1>
+        <CardList />
+      </div>
+    );
+  }
 }
