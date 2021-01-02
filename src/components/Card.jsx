31f09d48f622cd1b68729cab8b61dd65e4f48541
Trainee 18 <sagar.vartak@contentstack.com>
2021-01-02 15:03:48 +0530

    added social icons on the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import cardStyle from "../styles/Card.module.css";
+import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
 
 export default class Card extends Component {
   state = {
@@ -9,21 +10,36 @@ export default class Card extends Component {
 
   render() {
     return (
-      <Link to={"/profile/" + this.props.employee.employeeId}>
+      <div>
         <div className={cardStyle.box}>
-          <img
-            src={this.props.employee.imageUrl}
-            alt={this.props.employee.firstName}
-            className={cardStyle.imgWidth}
-          />
-          <h1>
-            {this.props.employee.firstName} {this.props.employee.lastName}
-          </h1>
-          <p className={cardStyle.title}>
-            Company: {this.props.employee.company}
-          </p>
+          <div>
+            <Link to={"/profile/" + this.props.employee.employeeId}>
+              <img
+                src={this.props.employee.imageUrl}
+                alt={this.props.employee.firstName}
+                className={cardStyle.imgWidth}
+              />
+              <h1>
+                {this.props.employee.firstName} {this.props.employee.lastName}
+              </h1>
+              <p className={cardStyle.title}>{this.props.employee.company}</p>
+            </Link>
+          </div>
+          <div className={cardStyle.linkStyle}>
+            <p className={cardStyle.links}>
+              <a href={this.props.employee.socialLinks[0].gitHub}>
+                <FaGithub size="2em" />
+              </a>
+              <a href={this.props.employee.socialLinks[0].linkedIn}>
+                <FaLinkedin size="2em" color="#0e76a8" />
+              </a>
+              <a href={this.props.employee.socialLinks[0].website}>
+                <FaGlobe size="2em" color=" #495e7b" />
+              </a>
+            </p>
+          </div>
         </div>
-      </Link>
+      </div>
     );
   }
 }
