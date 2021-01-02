00ed33574906dadafc6542dfe2a5290cdc85ba76
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-02 11:51:10 +0530

    fixed frontend

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,5 +1,6 @@
 const url =
   "https://trainee-profile-backend.herokuapp.com/employees/addEmployee";
+// const url = "http://localhost:3001/employees/addEmployee";
 export default url;
 // http://localhost:3001/employees/addEmployee
 // http://localhost:4000/employees/addEmployee
