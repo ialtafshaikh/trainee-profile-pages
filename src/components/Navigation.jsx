8aa64d64384512219d41d8bf07769aa46c22766d
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-03 20:57:11 +0530

    profiles link added in navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,21 +4,21 @@ import NavigationStyles from "../styles/Navigation.module.css";
 import { FaHome, FaRegIdCard, FaPlus } from "react-icons/fa";
 
 export default class Navigation extends Component {
-	render() {
-		return (
-			<div>
-				<p id={NavigationStyles.menu}>
-					<Link to="/">
-						<FaHome title="Home" />
-					</Link>
-					<Link to="/detail">
-						<FaRegIdCard title="Employee Details" />
-					</Link>
-					<Link to="/form">
-						<FaPlus title="Employee Add" />
-					</Link>
-				</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <p id={NavigationStyles.menu}>
+          <Link to="/">
+            <FaHome title="Home" />
+          </Link>
+          <Link to="/profiles">
+            <FaRegIdCard title="Employee Details" />
+          </Link>
+          <Link to="/form">
+            <FaPlus title="Employee Add" />
+          </Link>
+        </p>
+      </div>
+    );
+  }
 }
