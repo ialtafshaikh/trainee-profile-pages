e4367030c58e9209447fc905e876ffb1df47e608
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-03 20:54:45 +0530

    detail route changed to profiles

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //pages
 import Home from "../pages/Index";
-import Detail from "../pages/Detail";
+import Profiles from "../pages/Profiles";
 import Form from "../pages/Form";
 
 //components
@@ -15,24 +15,20 @@ import Footer from "../components/Footer";
 import CardDetail from "../components/CardDetail";
 
 export default class Router extends Component {
-	render() {
-		return (
-			<div>
-				<BrowserRouter>
-					<Header />
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/detail" exact component={Detail} />
-						<Route
-							path="/profile/:employeeId"
-							exact
-							component={CardDetail}
-						/>
-						<Route path="/form" exact component={Form} />
-					</Switch>
-				</BrowserRouter>
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/profiles" exact component={Profiles} />
+            <Route path="/profile/:employeeId" exact component={CardDetail} />
+            <Route path="/form" exact component={Form} />
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </div>
+    );
+  }
 }
